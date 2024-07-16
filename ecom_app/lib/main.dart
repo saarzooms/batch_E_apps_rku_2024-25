39bc1fbfe5c6f3966b1a9b35a95667ba5244b892
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-07-16 11:28:46 +0530

    splash screen logic and design

--- ecom_app/lib/main.dart
+++ ecom_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/login_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ ecom_app/lib/screens/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                'Ecom - App',
+                style: TextStyle(
+                  fontSize: 60,
+                ),
+              ),
+            ),
+          ),
+          Text('Developed By RKU Students !!!')
+        ],
+      ),
+    );
+  }
+}
