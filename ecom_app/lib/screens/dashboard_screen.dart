41b834efb3005439628b856f6d13939937239dcf
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-07-30 10:39:09 +0530

    todoapp + ecom icon

--- ecom_app/lib/screens/dashboard_screen.dart
+++ ecom_app/lib/screens/dashboard_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  String uname = "";
+  @override
+  void initState() {
+    super.initState();
+    fetchUser();
+  }
+
+  fetchUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    uname = await prefs.getString('uname') ?? "";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +28,7 @@ class DashboardScreen extends StatelessWidget {
         title: Text('Dashboard'),
       ),
       body: Container(
-        child: Text('Nothing to display'),
+        child: Text('Welcome $uname'),
       ),
     );
   }
