4ee411f8e125d53bedcaef97794775a4c3ca0a0a
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-07-30 10:59:50 +0530

    design of todo list

--- todo_app/lib/screens/todo_screen.dart
+++ todo_app/lib/screens/todo_screen.dart
@@ -10,6 +10,53 @@ class TodoScreen extends StatefulWidget {
 class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Todo App'),
+        ),
+        body: Column(
+          children: [
+            //to add/update
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(hintText: 'Enter task name'),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.add),
+                ),
+              ],
+            ),
+            //to display the list
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) => CheckboxListTile(
+                  onChanged: (v) {},
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: false,
+                  title: Text('Hi'),
+                  secondary: SizedBox(
+                    width: 80,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.edit),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.delete),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
